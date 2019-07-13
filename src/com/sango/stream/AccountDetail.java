b22438d015a02639f3426fc1d44c5ecfb7f48ed8
Sangopak <sangojumech07@gmail.com>
2019-07-13 13:00:16 -0400

    Java Stream examples

---
+++ src/com/sango/stream/AccountDetail.java
@@ -0,0 +1,61 @@
+package com.sango.stream;
+
+import java.util.List;
+import java.util.Set;
+
+public class AccountDetail {
+
+	private List<String> accountName;
+	private int accountAge;
+	private String accountStatus;
+	private String accountHolderFullName;
+	
+	public AccountDetail(List<String> accountName, int accountAge, String accountStatus, String accountHolderFullName) {
+		super();
+		this.accountName = accountName;
+		this.accountAge = accountAge;
+		this.accountStatus = accountStatus;
+		this.accountHolderFullName = accountHolderFullName;
+	}
+
+	public List<String> getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(List<String> accountName) {
+		this.accountName = accountName;
+	}
+
+	public int getAccountAge() {
+		return accountAge;
+	}
+
+	public void setAccountAge(int accountAge) {
+		this.accountAge = accountAge;
+	}
+
+	public String getAccountStatus() {
+		return accountStatus;
+	}
+
+	public void setAccountStatus(String accountStatus) {
+		this.accountStatus = accountStatus;
+	}
+
+	public String getAccountHolderFullName() {
+		return accountHolderFullName;
+	}
+
+	public void setAccountHolderFullName(String accountHolderFullName) {
+		this.accountHolderFullName = accountHolderFullName;
+	}
+
+	@Override
+	public String toString() {
+		return "AccountDetail [accountName=" + accountName + ", accountAge=" + accountAge + ", accountStatus="
+				+ accountStatus + ", accountHolderFullName=" + accountHolderFullName + "]";
+	}
+	
+	
+	
+}
---
+++ src/com/sango/stream/StreamMain.java
@@ -0,0 +1,80 @@
+package com.sango.stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamMain {
+
+	public static void main(String[] args) {
+		List<AccountDetail> listAccountDetail = new ArrayList<>();
+
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Freedom Card","Auto Loan"), 15, "Open", "Iron Man"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Saphaire Card", "Home Loan"), 4, "Closed", "Captain America"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Unlimited Freedom Card","AMEX"), 3, "Open", "The Hulk"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Freedom Card","City"), 8, "Closed", "Dr Strange"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Saphaire Card","Capitol One"), 2, "OPEN", "Antman"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Unlimited Freedom Card", "Auto Loan"), 1, "Open", "Spriderman"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Freedom Card","AMEX"), 3, "Open", "Widow"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Freedom Card","Amex"), 8, "Closed", "Thor"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Saphaire Card","BoA","Capitol One"), 2, "OPEN", "Hawk Eye"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Unlimited Freedom Card", "Auto Loan"), 1, "Open", "Captian Marvel"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Saphaire Reserve", "Auto Loan"), 20, "Open", "Nick Fury"));
+		listAccountDetail.add(new AccountDetail(Arrays.asList("Chase Unlimited Freedom Card","Chase Saphaire Reserve", "Auto Loan"), 19, "Open", "Star Lord"));
+		
+		
+		List<AccountDetail> openAccountList = listAccountDetail.stream()
+				.filter(accountDetail -> accountDetail.getAccountStatus().equalsIgnoreCase("Open"))
+				.collect(Collectors.toList());
+
+		// openAccountList.forEach(list -> System.out.println(list));
+		System.out.println("**********");
+
+		List<AccountDetail> openAccountAgeSortedList = listAccountDetail.stream()
+				.filter(accountDetail -> accountDetail.getAccountStatus().equalsIgnoreCase("Open"))
+				.sorted(Comparator.comparing(AccountDetail::getAccountAge)).collect(Collectors.toList());
+
+		// openAccountAgeSortedList.forEach(list -> System.out.println(list));
+		System.out.println("**********");
+
+		List<AccountDetail> openAccountAgeReverseSortedList = listAccountDetail.stream()
+				.filter(accountDetail -> accountDetail.getAccountStatus().equalsIgnoreCase("Open"))
+				.sorted(Comparator.comparing(AccountDetail::getAccountAge).reversed()).collect(Collectors.toList());
+
+		//openAccountAgeReverseSortedList.forEach(list -> System.out.println(list));
+
+		System.out.println("**********");
+
+		List<String> openAgedAccountHolderNameUpperCase = listAccountDetail.stream()
+				.filter(accountDetail -> accountDetail.getAccountStatus().equalsIgnoreCase("Open"))
+				.filter(accountDetail -> accountDetail.getAccountAge() > 10)
+				.map(accountDetail -> accountDetail.getAccountHolderFullName().toUpperCase())
+				.collect(Collectors.toList());
+		
+		//openAgedAccountHolderNameUpperCase.forEach(list -> System.out.println(list));
+		
+		System.out.println("**********");
+		
+		List<AccountDetail> agedAccountDetailParallel = listAccountDetail.parallelStream()
+				.filter(accountDetail -> accountDetail.getAccountAge() > 10)
+				.sorted(Comparator.comparing(AccountDetail::getAccountAge))
+				.collect(Collectors.toList());		
+						
+		agedAccountDetailParallel.forEach(System.out::println);
+		
+		System.out.println("**********");
+		
+		List<String> flatMapOpenAccountNameSortedList = listAccountDetail.stream()
+				.map(accountDetail -> accountDetail.getAccountName())
+				.flatMap(accountName -> accountName.stream())
+				.sorted(Comparator.comparing(String::toString))
+				.peek(System.out::println)
+				.collect(Collectors.toList());
+		
+		System.out.println("**********");
+		
+	}
+
+}
